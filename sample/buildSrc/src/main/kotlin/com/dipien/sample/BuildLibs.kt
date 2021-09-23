6c49e322bc00bf8d0fde29e2dbcd8bb120901b53
Maxi Rosson <maxirosson@gmail.com>
2021-09-23 14:53:51 -0300

    Added sample app

---
+++ sample/buildSrc/src/main/kotlin/com/dipien/sample/BuildLibs.kt
@@ -0,0 +1,10 @@
+package com.dipien.sample
+
+object BuildLibs {
+
+    // https://developer.android.com/studio/releases/gradle-plugin
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:7.0.2"
+
+    // https://github.com/JetBrains/kotlin/releases
+    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30"
+}
---
+++ sample/buildSrc/src/main/kotlin/com/dipien/sample/Libs.kt
@@ -0,0 +1,10 @@
+package com.dipien.sample
+
+object Libs {
+
+    // https://github.com/JetBrains/kotlin/releases
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.5.10"
+
+    // https://developer.android.com/jetpack/androidx/releases/appcompat
+    const val APPCOMPAT = "androidx.appcompat:appcompat:1.2.0"
+}

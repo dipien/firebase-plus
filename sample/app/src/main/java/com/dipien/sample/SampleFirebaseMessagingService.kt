b05cef0456abea42a12f60685290137ebe4a0ef8
Maxi Rosson <maxirosson@gmail.com>
2021-09-29 13:48:43 -0300

    Added SampleFirebaseMessagingService

---
+++ sample/app/src/main/java/com/dipien/sample/SampleFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.dipien.sample
+
+import com.dipien.firebase.messaging.AbstractFirebaseMessagingService
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SampleFirebaseMessagingService : AbstractFirebaseMessagingService()

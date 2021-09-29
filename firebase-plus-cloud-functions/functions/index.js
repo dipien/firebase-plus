af7f25c733a4975c3400f49fe6440c03f1a774d0
Maxi Rosson <maxirosson@gmail.com>
2021-09-29 15:15:02 -0300

    Added firebase-plus-cloud-functions

---
+++ firebase-plus-cloud-functions/functions/index.js
@@ -0,0 +1,21 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.pushConfig = functions.remoteConfig.onUpdate((versionMetadata) => {
+  // Create FCM payload to send data message to REMOTE_CONFIG_PUSH topic.
+  const payload = {
+    topic: "REMOTE_CONFIG_PUSH",
+    data: {
+      "REMOTE_CONFIG_STATUS": "STALE"
+    },
+    fcm_options: {
+      "analytics_label": "REMOTE_CONFIG_PUSH",
+    },
+  };
+  // Use the Admin SDK to send the ping via FCM.
+  return admin.messaging().send(payload).then((resp) => {
+    console.log(resp);
+    return null;
+  });
+});

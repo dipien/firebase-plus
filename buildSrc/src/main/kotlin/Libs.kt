197ffdf8c730be2b1a2047d7c4af7b85a344ffb5
dipien-ci <dipien2020@gmail.com>
2021-11-03 13:22:36 +0000

    Upgraded com.google.firebase:firebase-messaging from 22.0.0 to 23.0.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -2,7 +2,7 @@ object Libs {
 
     // https://firebase.google.com/support/release-notes/android
     const val FIREBASE_REMOTE_CONFIG = "com.google.firebase:firebase-config-ktx:21.0.1"
-    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:22.0.0"
+    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:23.0.0"
     const val FIREBASE_CRASHLYTICS = "com.google.firebase:firebase-crashlytics:18.2.4"
 
     // https://developer.android.com/jetpack/androidx/releases/preference
